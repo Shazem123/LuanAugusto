0256a0f5794b87b9be53df8731beed52a5ddf92a
Shazem123 <shazemstarve.io@gmail.com>
2025-12-27 16:11:40 -0300

    adição de message e comando while pra checar se a idade é valida

--- projetos/main2.py
+++ projetos/main2.py
@@ -1,22 +1,19 @@
 age = int(input("Welcome to the age and role identification program! Please enter your age: "))
 
+while age < 0:
+    age = int(input("Invalid age. Please enter a valid age: "))
+
 if age < 13:
     role = "Child"
+    message = "Enjoy your childhood!"
 elif age < 18:
     role = "Teenager"
+    message = "Take your time on your teenage years."
 elif age < 60:
     role = "Adult"
+    message = "You are in your prime. Grapple your responsibilites well."
 else:
     role = "Senior"
+    message = "You lived your life to its fullfilest and is now full of wisdom."
 
-if role == "Child":
-    print("Congratulations! You are classified as a Child. Enjoy your childhood!")
-
-elif role == "Teenager":
-    print("Great! You are classified as a Teenager. Embrace your teenage years!")
-
-elif role == "Adult":
-    print("You are classified as an Adult. Time to take on responsibilities!")
-
-else:
-    print("You are classified as a Senior. Enjoy the wisdom and experience you've gained!")
+print(f"You are classified as a {role}. {message}")
