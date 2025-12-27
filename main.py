928e7c2a9507f34e720b0d8ef4626bf124bd81a2
Shazem123 <shazemstarve.io@gmail.com>
2025-12-27 11:26:28 -0300

    adição de f strings nos inputs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print('Hello fellow human!')
 local = input('Where are you from?')
-personalinterest = input('Great, ' + local + ' is so incredible! What is your personal interest?')
-print('Man, you better go sleep now! You have a long day tomorrow doing ' + personalinterest + ' in ' + local + '!')
+personalinterest = input(f'Great, {local} is so incredible! What is your personal interest?')
+print(f'Man, you better go sleep now! You have a long day tomorrow doing {personalinterest} in {local}!')
